59469bbb51b16d81b58efe1a04b2a65feb949431
EJ9818 <emmanuel9818@gmail.com>
2022-07-24 00:45:47 -0500

    configure for run django in heroku

--- app/settings.py
+++ app/settings.py
@@ -32,6 +32,7 @@ INSTALLED_APPS = PROJECT_APPS + [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django',
+    'whitenoise',
 ]
 
 MIDDLEWARE = [
@@ -42,6 +43,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -116,9 +119,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# Update database configuration with $DATABASE_URL.
+import dj_database_url  
+db_from_env = dj_database_url.config(conn_max_age=500)  
+DATABASES['default'].update(db_from_env)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  
 STATIC_URL = '/static/'
-
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (  
-    os.path.join(BASE_DIR, 'app/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
--- app/wsgi.py
+++ app/wsgi.py
@@ -1,7 +1,8 @@
-import os
+import os  
+from django.core.wsgi import get_wsgi_application  
+from whitenoise.django import DjangoWhiteNoise
 
-from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
-
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "app.settings")  
+application = get_wsgi_application()  
+application = DjangoWhiteNoise(application)
